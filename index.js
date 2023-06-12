c923a3616af4bad6b7bc6cf2f0ce0220be6972fc
mehedi hasan <mdchanchal5675@gmail.com>
2023-06-12 08:35:21 +0600

    mongoDb set up DOne

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const express=require('express')
 require('dotenv').config()
 const jwt = require('jsonwebtoken')
@@ -6,14 +8,68 @@ const app=express()
 const port=process.env.PORT || 7000
 
 
+//==================
 //middleware
 app.use(cors())
 app.use(express.json())
+//===================
 
+
+//===================
 //testing server
 app.get('/',(req,res)=>{
     res.send('linguaCamp is running')
 })
+//====================
+
+
+//======================
+//MongoDb Configure start
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ruywwtc.mongodb.net/?retryWrites=true&w=majority`;
+
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    
+    await client.connect();
+
+//*********************************
+//all server routes here
+
+
+//*********************************
+
+
+
+
+
+
+
+
+
+
+
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    await client.close();
+  }
+}
+run().catch(console.dir);
+//======================
 
 
 
